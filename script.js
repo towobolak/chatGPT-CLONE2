a85281f07b48a417e22c3c26161288f7aa21a3d9
Towobola <towobolak@gmail.com>
2024-09-10 16:01:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.getElementById('toggleButton').addEventListener('click', function() {
+//     const form1 = document.getElementById('form1');
+//     const form2 = document.getElementById('form2');
+
+//     if (form1.style.display === 'none') {
+//       form1.style.display = 'block';
+//       form2.style.display = 'none';
+//     } else {
+//       form1.style.display = 'none';
+//       form2.style.display = 'block';
+//     }
+//   });
